9ca1f0eaf19454cda8beddad9c97591199bf4dcf
wangyufsn <dev.wangyu@proton.me>
2023-11-28 16:13:03 +0800

    up

---
+++ initExecutor.sh
@@ -0,0 +1,25 @@
+
+
+# nethermind \
+#   --log=INFO \
+#   --datadir=./execution-data \
+#   --Init.ChainSpecPath=./el-cl-genesis-data/custom_config_data/chainspec.json \
+#   --Init.WebSocketsEnabled=true \
+#   --Init.KzgSetupPath=./el-cl-genesis-data/custom_config_data/trusted_setup.txt \
+#   --config=none.cfg \
+#   --JsonRpc.Enabled=true \
+#   --JsonRpc.EnabledModules=net,eth,consensus,subscribe,web3,admin \
+#   --JsonRpc.Host=0.0.0.0 \
+#   --JsonRpc.Port=8545 \
+#   --JsonRpc.WebSocketsPort=8546 \
+#   --JsonRpc.EngineHost=0.0.0.0 \
+#   --JsonRpc.EnginePort=8551 \
+#   --Network.ExternalIp=127.0.0.1 \
+#   --Network.DiscoveryPort=50403 \
+#   --Network.P2PPort=50403 \
+#   --JsonRpc.JwtSecretFile=./el-cl-genesis-data/jwt/jwtsecret \
+#   --Network.OnlyStaticPeers=true \
+#   --Metrics.Enabled=true \
+#   --Metrics.ExposePort=9001 \
+#   --Network.StaticPeers=enode://30674d1f5fa3cf763c2d991eeaf585db42c45737a4d47aee3653b50ac33947437769e13f04c12bab63d5c2a416d08b2637e6340d13b7d59489f0ac8dae919756@88.99.94.109:50303
+
